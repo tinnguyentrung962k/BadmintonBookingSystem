a085554d09271aafec3d79f3326d10cc65d82534
tinnguyentrung962k <trungtin9620@gmail.com>
2024-07-12 04:33:11 +0700

    proccesing timeslot

---
+++ BadmintonBookingSystem.Repository/Repositories/TimeSlotRepository.cs
@@ -0,0 +1,18 @@
+﻿using BadmintonBookingSystem.DataAccessLayer.Context;
+using BadmintonBookingSystem.DataAccessLayer.Entities;
+using BadmintonBookingSystem.Repository.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadmintonBookingSystem.Repository.Repositories
+{
+    public class TimeSlotRepository : GenericRepository<TimeSlotEntity, string>, ITimeSlotRepository
+    {
+        public TimeSlotRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ BadmintonBookingSystem.Service/Services/Interface/ITimeSlotService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadmintonBookingSystem.Service.Services.Interface
+{
+    public interface ITimeSlotService
+    {
+        Task AddTimeSlotForCourt(string courtId);
+        Task RemoveTimeSlotForCourt(string courtId);
+
+    }
+}
