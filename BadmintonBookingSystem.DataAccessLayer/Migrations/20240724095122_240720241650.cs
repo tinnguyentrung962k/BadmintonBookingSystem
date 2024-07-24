d0b371eceb4d3467b56b0479c6cab7ab6c8621f1
tinnguyentrung962k <trungtin9620@gmail.com>
2024-07-24 16:53:23 +0700

    add migration

---
+++ BadmintonBookingSystem.DataAccessLayer/Migrations/20240724095122_240720241650.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BadmintonBookingSystem.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _240720241650 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentStatus",
+                table: "Booking",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "Booking");
+        }
+    }
+}
