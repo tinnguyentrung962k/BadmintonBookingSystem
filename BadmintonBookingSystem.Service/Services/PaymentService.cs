cd6166afa6d7cb7aaa346968c24e32f2ad27dd6c
Nguyen Trung Tin <trungtin9620@gmail.com>
2024-07-25 21:36:35 +0700

    fix deploy

--- BadmintonBookingSystem.Service/Services/PaymentService.cs
+++ BadmintonBookingSystem.Service/Services/PaymentService.cs
@@ -35,6 +35,7 @@ namespace BadmintonBookingSystem.Service.Services
         private readonly IConfiguration _configuration;
         private readonly UserManager<UserEntity> _userManager;
         private readonly PayOsSetting payOSSetting;
+        private readonly string _websiteDomain;
         private PayOS _payOS;
         public PaymentService(IUnitOfWork unitOfWork, IBookingService bookingService, IConfiguration configuration, UserManager<UserEntity> userManager)
         {
@@ -49,6 +50,8 @@ namespace BadmintonBookingSystem.Service.Services
                 ChecksumKey = _configuration.GetValue<string>("PayOSChecksumKey")
             };
             _payOS = new PayOS(payOSSetting.ClientId, payOSSetting.ApiKey, payOSSetting.ChecksumKey);
+            _websiteDomain = _configuration.GetValue<string>("WebSiteDomain");
+
         }
         public async Task<string> PaymentWithPayOs(string bookingId)
         {
@@ -86,8 +89,8 @@ namespace BadmintonBookingSystem.Service.Services
                 (int)booking.TotalPrice,
                 content,
                 bookings,
-                "https://localhost:7138/api/payment/cancel",
-                "https://localhost:7138/api/payment/return",
+                $"{_websiteDomain}/api/payment/cancel",
+                $"{_websiteDomain}/api/payment/return",
                 null,
                 user.FullName,
                 user.Email,
